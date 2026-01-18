a2aaee03af0e633df594ffd90fddfd4d243caa57
CodeverseOnGit <Codeverse1704@outlook.com>
2026-01-18 23:10:00 +0300

    first commit

---
+++ src/test/java/com/example/SWEBackend/SweBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SWEBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
